637a722ae581952b1d88b33f2b49a1fb49764529
Victor Kalinkin <vakalinkin@hotmail.com>
2021-05-24 15:04:17 -0700

    image src url gets updated from user input

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
 /* global data */
 /* exported data */
+
+var $entryPhotoUrl = document.querySelector('#entryPhotoUrl');
+var $img = document.querySelector('img');
+// console.log('$entryPhotoUrl:', $entryPhotoUrl);
+// console.log('$img', $img);
+// var $entryForm = document.querySelector()
+
+// var $entryForm = document.querySelector(".form");
+
+function updateSrc(event) {
+  // var userUrl = $entryForm.elements.url.value;
+  var userUrl = $entryPhotoUrl.value;
+  // console.log('current image url:', userUrl);
+  $img.setAttribute('src', userUrl);
+  // console.log('$img.src:', $img.src);
+
+}
+
+$entryPhotoUrl.addEventListener('input', updateSrc);
